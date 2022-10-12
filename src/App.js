4572bbe66da69c213242e87977bc66f1c7bdcc78
MANASH <anandmanash321@gmail.com>
2022-10-12 18:29:36 +0530

    Initial weather app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Mainfile from './component/Mainfile';
+import LoadingBar from 'react-top-loading-bar';
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const [progress, setProgress] = useState(0);
+
+   return (
+    <>
+    <LoadingBar
+        color='#f11946'
+        progress={progress}
+        // onLoaderFinished={() => setProgress(0)}
+      />
+    <Mainfile setProgress={setProgress}/>
+    </>
   );
 }
 
